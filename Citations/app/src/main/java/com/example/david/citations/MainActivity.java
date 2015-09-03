0cf37c193a841debc7ca4950bd04d2b571f224e4
davidrodier <david_rodier@hotmail.com>
2015-09-03 13:38:21 -0400

    init

---
+++ Citations/app/src/main/java/com/example/david/citations/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.david.citations;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void afficherInfosBoutonImage(View button){
+        TextView _textZone = (TextView)findViewById(R.id._textZone);
+
+
+
+    }
+}
